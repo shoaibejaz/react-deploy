4316f8906265b4a7ae5c788307d0ac1d291cb29a
Shoaib Ejaz <shoaibejazabc@gmail.com>
2020-03-25 23:31:31 +0500

    Updates

---
+++ precache-manifest.81150a47cfa96d3e923605f28f987f99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb6943f0c80bc6aec55a3e7ffb1eafb",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "add14520086319065374",
+    "url": "/react-deploy/static/css/main.b4d532e6.chunk.css"
+  },
+  {
+    "revision": "b451c962521998ca2b46",
+    "url": "/react-deploy/static/js/2.38f1524e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.38f1524e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add14520086319065374",
+    "url": "/react-deploy/static/js/main.06d3e149.chunk.js"
+  },
+  {
+    "revision": "4a8bebede5974b130c4b",
+    "url": "/react-deploy/static/js/runtime-main.fb319748.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);

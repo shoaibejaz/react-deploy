4bd70dd02c7682c059153ab785a69adaf372228b
Shoaib Ejaz <shoaibejazabc@gmail.com>
2020-03-25 23:30:26 +0500

    pushing on github rect.js app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Shoaib Ejaz</h1>
+      <h1>Pushing on github by Shoaib Ejaz</h1>
+    </div>
+  );
+}
+
+export default App;
